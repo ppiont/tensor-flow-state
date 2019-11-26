1c028e2130eecf6cfee2c83927190eea79bc335d
Peter Piontek <piontek0@gmail.com>
2019-11-26 12:47:36 +0100

    LSTM generator getting there

--- tensor-flow-state/modules/1_LSTM.py
+++ tensor-flow-state/modules/1_LSTM.py
@@ -48,21 +48,21 @@ df.set_index('timestamp', inplace = True, drop = True)
 
 # Add speed limit information
 df['speed_limit'] = np.where((df.index.hour < 19) & (df.index.hour >= 6), 100, 120)
-df['speed_cat_norm'] = df.speed - df.speed_limit
-df['speed_std_scaled'] = (df.speed - df.speed.mean()) / df.speed.std()
-df.speed_limit = pd.Categorical(df.speed_limit, categories=[100, 120])
+mean100 , mean120 = df.groupby(['speed_limit']).mean().unstack().values
+sd100 , sd120 = df.groupby(['speed_limit']).std().unstack().values
 
-# df.speed.hist(bins = df.speed.max() + 1)
-# df.speed_cat_norm.hist(bins = df.speed.max() + 1)
-# df.speed_std_scaled.hist(bins = df.speed.max() + 1)
+df['speed_normalized'] = np.where(df.speed_limit == 100, (df.speed - mean100) / sd100, (df.speed - mean120) / sd120)
 
+df.speed_normalized.hist(bins = df.speed.max() + 1)
 
 # example
 # new month (november) is test
 # jun-oct is train/val
 
+arr = np.array(df['speed_normalized'])
 
-arr = np.array(df['speed_cat_norm'])
+arr2 = np.array(np.linspace(1, 100, 100))
+print(arr2)
 
 def generator(data, lookback, delay, min_index = 0, max_index = None, 
               shuffle = False, batch_size = 128, step = 1):
@@ -81,43 +81,62 @@ def generator(data, lookback, delay, min_index = 0, max_index = None,
             rows = np.arange(i, min(i + batch_size, max_index))
             i += len(rows)
             
-    samples = np.zeros((len(rows),
-                        lookback // step,
-                        data.shape[-1]))
-    targets = np.zeros((len(rows),))
-    for j, row in enumerate(rows):
-        indices = range(rows[j] - lookback, rows[j] - step)
-        samples[j] = data[indices]
-        targets[j] = data[rows[j] + delay][1]
-    yield samples, targets
+        samples = np.zeros((len(rows),
+                            lookback // step,
+                            np.shape(data)[-1]))
+        targets = np.zeros((len(rows),))
+        for j, row in enumerate(rows):
+            indices = range(rows[j] - lookback, rows[j], step)
+            samples[j] = data[indices]
+            targets[j] = data[rows[j] + delay][1]
+        yield samples, targets
+
+data = np.array(df['speed_normalized'])
+lookback = 10
+delay = 10
+min_index = 0
+max_index = len(data) - 31 * 24 * 60 - 1
+min_index_val = len(data) - 31 * 24 * 60
+max_index_val = len(data) - 1
+step = 1
+batch_size = 128
+
+train_gen = generator(data,
+                      lookback = lookback,
+                      delay = delay,
+                      min_index = min_index,
+                      max_index = max_index,
+                      shuffle = False,
+                      step = step, 
+                      batch_size = batch_size)
+
+
+val_gen = generator(data,
+                    lookback = lookback,
+                    delay = delay,
+                    min_index = min_index_val,
+                    max_index = max_index_val,
+                    step = step,
+                    batch_size = batch_size)
+
+# ### SET UP MODEL ###
+# model = Sequential([
+#     TimeDistributed(Conv1D(filters=64, kernel_size=1, activation='relu',
+#                            input_shape=(None, n_steps, n_features))),
+#     TimeDistributed(MaxPooling1D(pool_size=2)),
+#     TimeDistributed(Flatten()),
+#     LSTM(50, activation='relu'),
+#     Dense(1)
+#     ])
     
+# # compile the model
+# model.compile(optimizer='adagrad', loss='mse')
 
+# # fit the model
+# model.fit(X, y, epochs=10, verbose=1, validation_data=(Xt, yt))
 
-samples, targets = generator(data = arr, lookback = 10, delay = 10, max_index = len(arr) - 60 * 24 * 31)
+# # show a summary of the model architecture
+# model.summary()
 
-
-
-
-
-
-### SET UP MODEL ###
-model = Sequential([
-    TimeDistributed(Conv1D(filters=64, kernel_size=1, activation='relu',
-                           input_shape=(None, n_steps, n_features))),
-    TimeDistributed(MaxPooling1D(pool_size=2)),
-    TimeDistributed(Flatten()),
-    LSTM(50, activation='relu'),
-    Dense(1)
-    ])
-    
-# compile the model
-model.compile(optimizer='adagrad', loss='mse')
-
-# fit the model
-model.fit(X, y, epochs=10, verbose=1, validation_data=(Xt, yt))
-
-# show a summary of the model architecture
-model.summary()
-
-yhat = model.predict(Xt)
+# yhat = model.predict(Xt)
 
