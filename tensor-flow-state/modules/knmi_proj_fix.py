86143535d8c2db6840d36f93085d8098fb2524e6
Peter Piontek <piontek0@gmail.com>
2019-12-02 22:51:08 +0100

    no progress with knmi proj

--- tensor-flow-state/modules/knmi_proj_fix.py
+++ tensor-flow-state/modules/knmi_proj_fix.py
@@ -35,37 +35,46 @@ plt.rcParams['image.cmap'] = 'viridis'
 
 
 
-# Establish db connection
-connection = {'host': 'http://leda.geodan.nl:8123'}
-# Load KNMI data from db 
-knmi_raw = read_clickhouse("SELECT x, y FROM knmi.MFBSNL25_05m GROUP BY x, y", connection = connection)
+# # Establish db connection
+# connection = {'host': 'http://leda.geodan.nl:8123'}
+# # Load KNMI data from db 
+# knmi_raw = read_clickhouse("SELECT x, y FROM knmi.MFBSNL25_05m GROUP BY x, y", connection = connection)
 # ndw = read_clickhouse("SELECT x, y FROM ndw.mstpoints GROUP BY x, y", connection = connection)
 
-# Convert knmi to GeoDataFrame
-knmi = gpd.GeoDataFrame(knmi_raw, geometry = gpd.points_from_xy(knmi_raw.x, knmi_raw.y))
 # Load NDW data
 ndw = gpd.read_file(os.path.join(datadir, "ndw_shapefile/Telpunten_WGS84.shp"), layer='Telpunten_WGS84')
+# Load knmi data
+knmi = gpd.read_file(os.path.join(datadir, 'knmi_gdf.geojson'))
+knmi.crs = "+proj=stere +lat_0=90 +lon_0=0.0 +lat_ts=60.0 +a=6378.137 +b=6356.752 +x_0=0 +y_0=0"
+
+
+# Convert knmi to GeoDataFrame
+# knmi = gpd.GeoDataFrame(knmi_raw, geometry = gpd.points_from_xy(knmi_raw.x, knmi_raw.y))
+
 
 # plot
-fig, ax = plt.subplots(figsize = (15, 15))
+# fig, ax = plt.subplots(figsize = (15, 15))
 plt.rcParams['agg.path.chunksize'] = 10000
 knmi.plot()
 ndw.plot()
 
+ndw.crs
 knmi.crs
-knmi.crs = "+proj=stere +lat_0=90 +lon_0=0.0 +lat_ts=60.0 +a=6378.137 +b=6356.752 +x_0=0 +y_0=0"
 
+test = ndw.to_crs({'init': 'epsg:28992'})
+test2 = knmi.to_crs({'init': 'epsg:28992'})
+test.plot()
 knmi = knmi.to_crs({'init': 'epsg:4326'})
-knmi = knmi.to_crs("+proj=longlat +a=3396000 +b=3396000")
-
+knmi = knmi.to_crs()
+"+proj=longlat +a=3396000 +b=3396000"
+"+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs"
 
 import numpy as np
-
+import pyproj
 x = np.array(knmi['geometry'][:])
 y = np.array(knmi.variables['y'][:])
 xv,  yv = np.meshgrid(x, y)
-p = pyproj.Proj("+proj=stere +lat_0=90 +lat_ts=60 +lon_0=-105 +k=90 
-            +x_0=0 +y_0=0 +a=6371200 +b=6371200 +units=m +no_defs")
+p = pyproj.Proj("+proj=stere +lat_0=90 +lon_0=0.0 +lat_ts=60.0 +a=6378.137 +b=6356.752 +x_0=0 +y_0=0")
 lons, lats = p(xv, yv, inverse=True)
 
 
@@ -81,7 +90,14 @@ print(x)
 # knmi.to_file(os.path.join(datadir, "knmi_gdf.geojson"), driver='GeoJSON')
 
 
-
+## idiot proof test directly with pyproj (also fails)
+# # define the projections
+# p1 = pyproj.Proj("+proj=stere +lat_0=90 +lon_0=0.0 +lat_ts=60.0 +a=6378.137 +b=6356.752 +x_0=0 +y_0=0")
+# p2 = pyproj.Proj(proj='latlong', datum='WGS84')
+ 
+# # Transform point (155000.0, 446000.0) with EPSG:28992
+# lon, lat, z = pyproj.transform(p1, p2, 0.0, 320, 0.0)
+# print(lon, lat, z) # 5.38720294616 52.0023756348 43.6057764404
 
 
 
